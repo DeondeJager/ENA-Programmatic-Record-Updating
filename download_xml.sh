baf97b249382842285b512b90ad41ee9ff3b46d1
Deon de Jager <dejager4@gmail.com>
2026-04-22 15:15:15 +0200

    Update authorization header in download_xml.sh

--- download_xml.sh
+++ download_xml.sh
@@ -7,12 +7,12 @@ ACCESSION=$1
 #curl -X 'GET' \
 #  "https://www.ebi.ac.uk/ena/submit/webin-v2/run/${ACCESSION}" \
 #  -H 'accept: application/xml' \
-#  -H 'Authorization: Basic V2ViaW4tNjUwOTc6b3p2NWQ2cVVNeEFxYWk=' \
+#  -H 'Authorization: Basic <insert 36 character authorization code>' \
 #  > ${ACCESSION}.xml
 
 # Download experiment xml (uncomment if needed)
 curl -X 'GET' \
   "https://www.ebi.ac.uk/ena/submit/webin-v2/experiment/${ACCESSION}" \
   -H 'accept: application/xml' \
-  -H 'Authorization: Basic V2ViaW4tNjUwOTc6b3p2NWQ2cVVNeEFxYWk=' \
+  -H 'Authorization: Basic <insert 36 character authorization code>' \
   > ${ACCESSION}.xml
