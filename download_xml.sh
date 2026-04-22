6dc930a9ef6179875dffc0687157f28a300aa1fc
DeondeJager <dejager4@gmail.com>
2026-04-22 15:13:42 +0200

    Add experiment download code to script for convenience

--- download_xml.sh
+++ download_xml.sh
@@ -3,9 +3,16 @@
 # Define first argument as ACCESSION
 ACCESSION=$1
 
-# Download run xml
+# Download run xml (uncomment if needed)
+#curl -X 'GET' \
+#  "https://www.ebi.ac.uk/ena/submit/webin-v2/run/${ACCESSION}" \
+#  -H 'accept: application/xml' \
+#  -H 'Authorization: Basic V2ViaW4tNjUwOTc6b3p2NWQ2cVVNeEFxYWk=' \
+#  > ${ACCESSION}.xml
+
+# Download experiment xml (uncomment if needed)
 curl -X 'GET' \
-  "https://www.ebi.ac.uk/ena/submit/webin-v2/run/${ACCESSION}" \
+  "https://www.ebi.ac.uk/ena/submit/webin-v2/experiment/${ACCESSION}" \
   -H 'accept: application/xml' \
-  -H 'Authorization: Basic <insert 36 character authorization code>' \
+  -H 'Authorization: Basic V2ViaW4tNjUwOTc6b3p2NWQ2cVVNeEFxYWk=' \
   > ${ACCESSION}.xml
